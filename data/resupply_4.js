3e7ee17f727d5c80085b6cc3b8008c41af7f0537
Dylan <dylan@thisis-notmyemail.com>
2020-06-23 12:21:59 -0600

    resupply

---
+++ data/resupply_4.js
@@ -0,0 +1,26 @@
+var json_resupply_4 = {
+"type": "FeatureCollection",
+"name": "resupply_4",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": "1", "name": "Conifer" }, "geometry": { "type": "Point", "coordinates": [ -105.305478527255516, 39.520474180766307 ] } },
+{ "type": "Feature", "properties": { "id": "2", "name": "Buffalo Creek" }, "geometry": { "type": "Point", "coordinates": [ -105.271897410072583, 39.387063483930611 ] } },
+{ "type": "Feature", "properties": { "id": "3", "name": "Bailey" }, "geometry": { "type": "Point", "coordinates": [ -105.473840999118394, 39.406252693749444 ] } },
+{ "type": "Feature", "properties": { "id": "4", "name": "Jefferson" }, "geometry": { "type": "Point", "coordinates": [ -105.800457341243117, 39.377211880675389 ] } },
+{ "type": "Feature", "properties": { "id": "5", "name": "Fairplay" }, "geometry": { "type": "Point", "coordinates": [ -105.993548765045134, 39.221642215358422 ] } },
+{ "type": "Feature", "properties": { "id": "6", "name": "Frisco" }, "geometry": { "type": "Point", "coordinates": [ -106.092693015775751, 39.574243945779486 ] } },
+{ "type": "Feature", "properties": { "id": "7", "name": "Breckenridge" }, "geometry": { "type": "Point", "coordinates": [ -106.045633763124812, 39.485608071854401 ] } },
+{ "type": "Feature", "properties": { "id": "8", "name": "Copper Mountain" }, "geometry": { "type": "Point", "coordinates": [ -106.144920790714224, 39.501456303177982 ] } },
+{ "type": "Feature", "properties": { "id": "8", "name": "Leadville" }, "geometry": { "type": "Point", "coordinates": [ -106.288440089150939, 39.246342611925819 ] } },
+{ "type": "Feature", "properties": { "id": "9", "name": "Twin Lakes" }, "geometry": { "type": "Point", "coordinates": [ -106.382358706850582, 39.082234890454338 ] } },
+{ "type": "Feature", "properties": { "id": "10", "name": "Buena Vista" }, "geometry": { "type": "Point", "coordinates": [ -106.129443779223479, 38.844882640433276 ] } },
+{ "type": "Feature", "properties": { "id": "11", "name": "Mount Princeton" }, "geometry": { "type": "Point", "coordinates": [ -106.162425233599606, 38.733116915387306 ] } },
+{ "type": "Feature", "properties": { "id": "12", "name": "Poncha Springs" }, "geometry": { "type": "Point", "coordinates": [ -106.077358781143801, 38.515010985928583 ] } },
+{ "type": "Feature", "properties": { "id": "13", "name": "Saguache" }, "geometry": { "type": "Point", "coordinates": [ -106.142293696512866, 38.088308065805329 ] } },
+{ "type": "Feature", "properties": { "id": "14", "name": "Gunnison" }, "geometry": { "type": "Point", "coordinates": [ -106.925624654474589, 38.546964446921564 ] } },
+{ "type": "Feature", "properties": { "id": "15", "name": "Creede" }, "geometry": { "type": "Point", "coordinates": [ -106.926652647857736, 37.849984933144647 ] } },
+{ "type": "Feature", "properties": { "id": "16", "name": "Lake City" }, "geometry": { "type": "Point", "coordinates": [ -107.315330521068802, 38.030076523955536 ] } },
+{ "type": "Feature", "properties": { "id": "17", "name": "Silverton" }, "geometry": { "type": "Point", "coordinates": [ -107.667386130005042, 37.808511825093092 ] } },
+{ "type": "Feature", "properties": { "id": "18", "name": "Molas Lake" }, "geometry": { "type": "Point", "coordinates": [ -107.681986848525128, 37.750349886961956 ] } }
+]
+}
